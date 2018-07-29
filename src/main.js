89b53ec00eef51c89aec19089bc451c98605a1ad
杨艾林 <747564962@qq.com>
2018-07-29 15:39:20 +0800

    调整进度条逻辑的位置，路由逻辑全由router负责

--- src/main.js
+++ src/main.js
@@ -4,23 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css'// progress bar style
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
-NProgress.configure({ showSpinner: false })// NProgress Configuration
-
-router.beforeEach((to, from, next) => {
-  NProgress.start() // start progress bar
-  next()
-  NProgress.done()
-})
-
-router.afterEach(() => {
-  NProgress.done() // finish progress bar
-})
 
 Vue.use(Element)
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import * as manifest from '../client'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css'// progress bar style
 Vue.use(Router)
 // 注入路由
 const routes = Object.keys(manifest)
   .filter(key => !!manifest[key].route)
   .map(key => manifest[key].route)
-export default new Router({
+let router = new Router({
   routes: [
     ...routes
   ]
 })
+
+NProgress.configure({ showSpinner: false })// NProgress Configuration
+
+// 加上进度条
+// 以后可以在这里做路由守卫
+router.beforeEach((to, from, next) => {
+  NProgress.start() // start progress bar
+  next()
+  // NProgress.done()
+})
+
+router.afterEach(() => {
+  NProgress.done() // finish progress bar
+})
+export default router
