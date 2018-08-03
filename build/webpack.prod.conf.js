04b8ebea6ac18266330782f696af6e9a089e97ab
杨艾林 <747564962@qq.com>
2018-08-03 15:08:31 +0800

    保存

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -4,6 +4,8 @@ const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
 const merge = require('webpack-merge')
+
+require('../config/dev.env') 
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,11 @@
 'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development'
+process.env.NODE_ENV = 'development'
 
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err
 })
