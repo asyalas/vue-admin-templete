1548bb0d6196fd4c4a1c4026614b8ce2f3bd1a04
杨艾林 <747564962@qq.com>
2018-08-03 15:04:06 +0800

    加上环境变量

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-
+const env = require('../config/env')
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -31,6 +31,9 @@ module.exports = {
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+  plugins: [
+    new webpack.DefinePlugin(env.stringified),
+  ],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
